df85f188055a4bd17b7005a6ee5c43ff461020e7
atiwari80 <ayush.tiwari2@mphasis.com>
2026-07-01 11:53:12 -0400

    fix: remediate vulnerability in app/client/cypress/scripts/cypress-local-setup.js

--- app/client/cypress/scripts/cypress-local-setup.js
+++ app/client/cypress/scripts/cypress-local-setup.js
@@ -1,2 +1,2 @@
-const spawn = require('child_process').spawn; const command = spawn(command, ["--inspect","--trace-deprecation"]);
- const command = spawn('node', [command]);
+const spawn = require('child_process').spawn; const options = { shell: true, stdio: 'inherit' }; const command = spawn(command, ["--inspect","--trace-deprecation"], options);
+ const command = spawn(process.execPath, [command]);
