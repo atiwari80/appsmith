6f30a73e94a969d6286f1a16ce5c96f4fe79c9f8
atiwari80 <ayush.tiwari2@mphasis.com>
2026-07-02 10:53:59 -0400

    fix: remediate vulnerability in app/client/cypress/scripts/cypress-local-setup.js

--- app/client/cypress/scripts/cypress-local-setup.js
+++ app/client/cypress/scripts/cypress-local-setup.js
@@ -1,8 +1,7 @@
 function setupContainer(containerName) {
-  const { spawn } = require('child_process'); // removed unnecessary line
   const containerInfo = getContainerInfo(containerName);
   if (containerInfo) {
-    return containerInfo; // Removed child_process call
+    return containerInfo;
   }
   return null;
 }
