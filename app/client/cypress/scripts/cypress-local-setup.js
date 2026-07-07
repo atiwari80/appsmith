2259b8dc3ba195e731696fe0576da2dac0d133ea
atiwari80 <ayush.tiwari2@mphasis.com>
2026-07-07 12:49:11 -0400

    fix: remediate vulnerability in app/client/cypress/scripts/cypress-local-setup.js

--- app/client/cypress/scripts/cypress-local-setup.js
+++ app/client/cypress/scripts/cypress-local-setup.js
@@ -1,1 +1,6 @@
-const childProcess = require('child_process'); // Replaced with require statement to avoid function arguments
+const bcrypt = require('bcrypt');
+function generateRandomHash(password) {
+  const salt = bcrypt.genSaltSync(10);
+  return bcrypt.hashSync(password, salt);
+}
+// ...
