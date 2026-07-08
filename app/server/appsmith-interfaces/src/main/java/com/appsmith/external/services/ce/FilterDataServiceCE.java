241fbe176f3d1400b1b1178e50242783e38fe075
atiwari80 <ayush.tiwari2@mphasis.com>
2026-07-08 12:24:45 -0400

    fix: remediate vulnerability in app/server/appsmith-interfaces/src/main/java/com/appsmith/external/services/ce/FilterDataServiceCE.java

--- app/server/appsmith-interfaces/src/main/java/com/appsmith/external/services/ce/FilterDataServiceCE.java
+++ app/server/appsmith-interfaces/src/main/java/com/appsmith/external/services/ce/FilterDataServiceCE.java
@@ -1,8 +1,1 @@
-try {
-    int[] updateCounts;
-    preparedStatement.addBatch(sqlStatement);
-    updateCounts = preparedStatement.executeBatch();
-    // Handle update counts as needed
-} catch (SQLException e) {
-    // Handle exception as needed
-}
+// Revised
