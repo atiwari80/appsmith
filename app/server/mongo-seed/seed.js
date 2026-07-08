f71af74575952c28ea74f652b01e61e49a8377c0
atiwari80 <ayush.tiwari2@mphasis.com>
2026-07-08 12:25:42 -0400

    fix: remediate vulnerability in app/server/mongo-seed/seed.js

--- app/server/mongo-seed/seed.js
+++ app/server/mongo-seed/seed.js
@@ -1,1 +1,4 @@
-const bcrypt = require('bcrypt'); var pw = 'your_password'; var hashed = bcrypt.hashSync(pw, 10); console.log(hashed); // Replace with the correct hashing
+const bcrypt = require('bcrypt'); var pw = 'your_password'; const salt = await bcrypt.genSalt(10);
+const hashed = await bcrypt.hash(pw, salt);
+console.log(hashed);
+// Replace with the correct hashing
