2d1361f67c9a48aae43758cda6c54005b0635965
atiwari80 <ayush.tiwari2@mphasis.com>
2026-07-15 11:37:11 -0400

    fix: remediate vulnerability in app/server/appsmith-interfaces/src/main/java/com/appsmith/external/services/ce/FilterDataServiceCE.java

--- app/server/appsmith-interfaces/src/main/java/com/appsmith/external/services/ce/FilterDataServiceCE.java
+++ app/server/appsmith-interfaces/src/main/java/com/appsmith/external/services/ce/FilterDataServiceCE.java
@@ -1,1 +1,6 @@
-preparedStatement.executeUpdate();
+    String query = "SELECT * FROM users WHERE " + "first_name" = ? AND " + "last_name" = ?;
+
+    PreparedStatement ps = conn.prepareStatement(query);
+    ps.setString(1, params.first_name);
+    ps.setString(2, params.last_name);
+    ResultSet rs = ps.executeQuery();
